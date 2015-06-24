d58ed85699a81e106336381095fd4cc3b936c939
Robert DiPietro <rdipietro@gmail.com>
2015-06-24 16:19:08 +0200

    Update release for pip

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     # default ('pyrvm')
     package_dir = {'pyrvm': ''},
     packages = ['pyrvm'],
-    version = '0.11',
+    version = '0.12',
     description = 'Ranking Vector Machines in Python',
     author = 'Robert DiPietro',
     author_email = 'rdipietro@gmail.com',
     url = 'https://github.com/rdipietro/pyrvm',
-    download_url = 'https://github.com/rdipietro/pyrvm/tarball/0.11',
-    install_requires = ['numpy', 'sklearn', 'pulp'],
+    download_url = 'https://github.com/rdipietro/pyrvm/tarball/0.12',
+    install_requires = ['numpy', 'scikit-learn', 'pulp'],
     keywords = [],
     classifiers = [],
 )
